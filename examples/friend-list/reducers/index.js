5d035515c9eb5fbd33363227ca755cb60ff01c76
Derek Cuevas <dccuevas2@gmail.com>
2016-04-01 16:40:43 -1000

    updated friend-list example

--- examples/friend-list/reducers/index.js
+++ examples/friend-list/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import createMeta from './meta';
+import createMeta from 'redux-meta-reducer';
 import * as types from '../constants/actionTypes';
 
 const initialState = {
